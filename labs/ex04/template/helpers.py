70fb7874b312c387889906dc0261655c14f9a230
Ludde <ludviggerdin@gmail.com>
2020-10-12 20:15:45 +0200

    Add help files

--- labs/ex04/template/helpers.py
+++ labs/ex04/template/helpers.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def load_data():
     """load data."""
-    data = np.loadtxt("dataEx3.csv", delimiter=",", skiprows=1, unpack=True)
+    data = np.loadtxt("template/dataEx3.csv", delimiter=",", skiprows=1, unpack=True)
     x = data[0]
     y = data[1]
     return x, y
--- labs/ex04/template/ridge_regression.py
+++ labs/ex04/template/ridge_regression.py
@@ -7,10 +7,20 @@ Ridge Regression
 import numpy as np
 
 
+def compute_ridge_loss(y, tx, w, lambda_):
+    """Compue RMSE loss in matrix form."""
+    e = y[np.newaxis].T - np.dot(tx, w[np.newaxis].T)
+    rmse = np.sqrt(1/(tx.shape[0]) * np.dot(e.T, e) + lambda_ * np.dot(w, w.T))
+
+    return rmse.ravel()
+
+
 def ridge_regression(y, tx, lambda_):
-    """implement ridge regression."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # ridge regression: TODO
-    # ***************************************************
-    raise NotImplementedError
+    """Implement ridge regression."""
+    a = (tx.T @ tx) + lambda_ * 2 * tx.shape[0] * np.eye(tx.shape[1])
+    b = tx.T @ y
+    w = np.linalg.solve(a, b)
+    rmse = compute_ridge_loss(y, tx, w, lambda_)
+
+    return rmse, w
+
